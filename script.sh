06dfc719db8271a9d8d6251cbfd837fa79e1299a
Clovis <clovis.peridy@akeneo.com>
2025-04-09 10:22:23 +0200

    comment & yes/no to org creation

--- script.sh
+++ script.sh
@@ -6,11 +6,12 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+##########################################################
 # Prompt the user for the API token
+##########################################################
 echo -ne "${YELLOW}Enter your API token: ${NC}"
 read API_TOKEN
 
-# Get the access token
 RESPONSE=$(curl -s -u platform-api-user: \
   -d "grant_type=api_token&api_token=$API_TOKEN" \
   https://auth.api.platform.sh/oauth2/token)
@@ -25,6 +26,9 @@ fi
 
 echo -ne "${GREEN}Access Token:${NC} $ACCESS_TOKEN\n"
 
+##########################################################
+# Check user phone verification
+##########################################################
 echo -ne "${YELLOW}Checking if your profile meet the requirement${NC}\n"
 
 # Check if user phone is verified
@@ -46,38 +50,66 @@ echo -ne "${GREEN}All good !${NC}\n"
 echo -ne "${YELLOW}Enter the owner ID: ${NC}"
 read OWNER_ID
 
-# Prompt the user for the organization name
-echo -ne "${YELLOW}Enter the name of the organisation (name must be lowercase, no space, no dash): ${NC}"
-read ORG_NAME
+##########################################################
+# Create a new organisation or use Akeneo POC organisation
+##########################################################
+echo -ne "${YELLOW}Do you want to create a new organisation or use Akeneo POC ? (y/n): ${NC}"
+read ANSWER
+echo
 
-# Check if values are empty
-if [ -z "$API_TOKEN" ] || [ -z "$OWNER_ID" ] || [ -z "$ORG_NAME" ]; then
-  echo "API token / owner ID / org name cannot be empty."
+ANSWER=$(echo "$ANSWER" | tr '[:upper:]' '[:lower:]')
+if [[ "$ANSWER" == "y" || "$ANSWER" == "yes" ]]; then
+  
+  ##########################################################
+  # User want to create a new organisation
+  ##########################################################
+
+  # Prompt the user for the organization name
+  echo -ne "${YELLOW}Enter the name of the organisation (name must be lowercase, no space, no dash): ${NC}"
+  read ORG_NAME
+
+  # Check if values are empty
+  if [ -z "$API_TOKEN" ] || [ -z "$OWNER_ID" ] || [ -z "$ORG_NAME" ]; then
+    echo "API token / owner ID / org name cannot be empty."
+    exit 1
+  fi
+
+  # Make the POST request to create the organization
+  ORG_RESPONSE=$(curl -s --location --request POST 'https://api.platform.sh/organizations' \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/json" \
+    --header "Authorization: Bearer $ACCESS_TOKEN" \
+    --data-raw "{
+      \"label\": \"$ORG_NAME\",
+      \"owner_id\": \"$OWNER_ID\",
+      \"name\": \"$ORG_NAME\",
+      \"country\": \"FR\"
+    }")
+
+  # Extract and display the organization ID
+  ORG_ID=$(echo "$ORG_RESPONSE" | jq -r '.id')
+
+  if [ "$ORG_ID" == "null" ] || [ -z "$ORG_ID" ]; then
+    echo "Failed to create organization"
+    echo "Response: $ORG_RESPONSE"
+    exit 1
+  fi
+
+  echo -ne "${GREEN}SUCCESS ! organisation ID: $ORG_ID ${NC}\n"
+
+else
+
+  ##########################################################
+  # User want to use Akeneo organisation
+  ##########################################################
+
+  echo -e "${GREEN}You chose to use Akeneo.${NC}"
   exit 1
 fi
 
-# Make the POST request to create the organization
-ORG_RESPONSE=$(curl -s --location --request POST 'https://api.platform.sh/organizations' \
-  --header "Content-Type: application/json" \
-  --header "Accept: application/json" \
-  --header "Authorization: Bearer $ACCESS_TOKEN" \
-  --data-raw "{
-    \"label\": \"$ORG_NAME\",
-    \"owner_id\": \"$OWNER_ID\",
-    \"name\": \"$ORG_NAME\",
-    \"country\": \"FR\"
-  }")
-
-# Extract and display the organization ID
-ORG_ID=$(echo "$ORG_RESPONSE" | jq -r '.id')
-
-if [ "$ORG_ID" == "null" ] || [ -z "$ORG_ID" ]; then
-  echo "Failed to create organization"
-  echo "Response: $ORG_RESPONSE"
-  exit 1
-fi
-
-echo -ne "${GREEN}SUCCESS ! organisation ID: $ORG_ID ${NC}\n"
+##########################################################
+# Create a new project
+##########################################################
 
 # Prompt for project title
 echo -ne "${YELLOW}Enter your project title: ${NC}"
@@ -133,6 +165,10 @@ if [ "$PROJECT_ID" == "null" ] || [ -z "$PROJECT_ID" ]; then
 fi
 
 echo -ne "Project ID: $PROJECT_ID"
+
+##########################################################
+# Create a new github integration
+##########################################################
 
 # Prompt for project title
 echo -ne "${YELLOW}Enter your repository name: ${NC}"
