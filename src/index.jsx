00802915bb038b130c89fabb50387a7de72c3c1d
JVerbrugge <julien.verbrugge@akeneo.com>
2026-04-07 16:09:36 +0200

    add link to iframe

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,13 @@ import IframeExtension from './components/IframeExtension';
 import { ThemeProvider } from 'styled-components';
 import { connectorTheme } from 'akeneo-design-system';
 
-const HomePage = () => <div>Hello world</div>;
+const HomePage = () => (
+  <div>
+    Hello world
+    <br />
+    <a href="/iframe-extension">Iframe Extension</a>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
